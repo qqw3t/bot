3c2b469b0a8c352f06dbffef304adcace5466679
qqw3t <ezoss617@gmail.com>
2024-08-26 04:35:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 def config_get(key):
     config = {
-        'token': os.getenv('TELEGRAM_BOT_TOKEN', '7365624478:AAEww_niTdh2E1q9iPS6vxCKgmlM9rXsHp0')
+        'token': os.getenv('TELEGRAM_BOT_TOKEN', '')
     }
     return config.get(key)
